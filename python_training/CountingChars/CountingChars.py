ab8b6337afacaeef56a741d542bb015e2d013295
Karol Kuberacki <karol.kubercki@outlook.com>
2022-03-19 20:44:05 +0100

    Rename of constance

--- python_training/CountingChars/CountingChars.py
+++ python_training/CountingChars/CountingChars.py
@@ -2,15 +2,15 @@ import collections
 
 print("Please wire a string")
 value = input("Write something")
-test = collections.Counter(value)
+cahrs_of_value = collections.Counter(value)
 
 
 def countetrs_string(value_of_string):
     value_of_string = {}
-    for key, value in test.items():
+    for key, value in cahrs_of_value.items():
         if value > 1:
             value_of_string[key] = value
     print(value_of_string)
 
 
-countetrs_string(test)
+countetrs_string(cahrs_of_value)
