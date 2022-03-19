827adbb67a41e7e1cb66a0598b0261441ec37764
Karol Kuberacki <karol.kubercki@outlook.com>
2022-03-19 19:46:01 +0100

    Add Fibonacii

---
+++ python_training/Fibonaci/Fibonaci.py
@@ -0,0 +1,12 @@
+print("Welcome in simply fibonacii founder")
+print("Standard fibonacii start for 0 \n This is start from 0")
+SelectCalculatorFunction = int(input("Please input searching fibonachi.."))
+
+
+def fibonaci_of(number):
+    if number in {1, 2}:
+        return number
+    return fibonaci_of(number - 1) + fibonaci_of(number - 2)
+
+
+print(fibonaci_of(SelectCalculatorFunction))
